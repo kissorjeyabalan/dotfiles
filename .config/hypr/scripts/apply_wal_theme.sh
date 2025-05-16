44b00a470fd60b1ea3c2c5076b4f46c2db6daad5
Kissor Jeyabalan <kij@kij.no>
2025-05-16 15:04:35 +0200

    feat(matugen): swap color scheme to matugen

--- .config/hypr/scripts/apply_wal_theme.sh
+++ .config/hypr/scripts/apply_wal_theme.sh
@@ -1,39 +1,23 @@
 #!/bin/bash
 
 THEME_FILE="/tmp/theme_variant"
-#wal_arguments=""
-
-#if [ -s "$THEME_FILE" ]; then
-#  case $(<"$THEME_FILE") in
-#    "light") wal_arguments="lighten -l" ;;
-#  esac
-#fi
-
-#wal -i ~/Pictures/wallpaper.png --cols16 $wal_arguments -q -n -e
-
-#pgrep -x "waybar" > /dev/null && killall -SIGUSR2 waybar
-
-#walogram -s > /dev/null
-#spicetify apply -q -n
-
-
 wal_arguments=""
 
 if [ -s "$THEME_FILE" ]; then
   case $(<"$THEME_FILE") in
-    "light") wal_arguments="--light --dark-offset 0.2" ;;
-    *) wal_arguments="--dark --bright-offset 0.4" ;;
+    "light") wal_arguments="-C ~/.config/wallust/wallust-light.toml" ;;
+    *) wal_arguments="-C ~/.config/wallust/wallust-dark.toml" ;;
   esac
 fi
 
-hellwal -i ~/Pictures/wallpaper.png $wal_arguments -q
+matugen image ~/Pictures/wallpaper.png $wal_arguments -q
+wallust run ~/Pictures/wallpaper.png -w -q
 
 
 # Restart waybar
 pgrep -x "waybar" > /dev/null && killall -SIGUSR2 waybar
 
 # Restart cava
-cp $HOME/.cache/hellwal/cava.conf $HOME/.config/cava/config
 pkill -USR2 cava # Reloads cava's colorscheme configuration
 
 spicetify apply -q -n
